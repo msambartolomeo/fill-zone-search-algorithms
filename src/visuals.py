c7dbd6c86ee7fe23773d0dba25fcc227185b4836
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-03-20 11:47:14 -0300

    Fix bugs

--- src/visuals.py
+++ src/visuals.py
@@ -1,19 +1,23 @@
 import networkx as nx
 from .data_structures import Node
 from queue import Queue
+from typing import Set
 import matplotlib.pyplot as plt
 
 
 def draw_graph(root: Node):
-    g: nx.Graph = nx.Graph()
     q: Queue[Node] = Queue(maxsize=0)
+    g: nx.Graph = nx.Graph()
     q.put(root)
+    visited: Set[Node] = {root}
+
     while not q.empty():
         curr_node = q.get()
         g.add_node(curr_node, color=curr_node.color)
         for n in curr_node.neighbors:
-            if n not in g.nodes:
+            if n not in visited:
                 q.put(n)
+                visited.add(n)
             g.add_edge(curr_node, n)
 
     nx.draw(g, with_labels=True)
